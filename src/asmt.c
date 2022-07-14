92ca7248774885b2b6a0b113a307c69a498fd41f
Stephen Morgan <smorgan@aerospike.com>
2022-07-14 14:19:09 -0700

    Changed release number to 1.0.0. (Yay!)

--- src/asmt.c
+++ src/asmt.c
@@ -128,7 +128,7 @@ typedef struct as_cmp_s {
 // Constant globals.
 
 static const char g_fullname[] = "Aerospike Shared Memory Tool";
-static const char g_version[] = "Version 0.9.4";
+static const char g_version[] = "Version 1.0.0";
 static const char g_copyright[] = "Copyright (C) 2022 Aerospike, Inc.";
 static const char g_all_rights[] = "All rights reserved.";
 
